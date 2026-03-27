c0bc486bb252caf41a11ac6cf111dbc7f3c6d6cd
Jorge Cordova <55065873+jcordovaj@users.noreply.github.com>
2026-03-27 05:39:20 -0300

    actualizar venv 311

--- alma-api/app/venv312/Lib/site-packages/meutils/init/__init__.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-# @Project      : AI.  @by PyCharm
-# @File         : __init__.py
-# @Time         : 2023/4/27 16:54
-# @Author       : betterme
-# @WeChat       : meutils
-# @Software     : PyCharm
-# @Description  :
